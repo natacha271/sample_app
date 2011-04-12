88cd41a51e0b503c3909548f1c89342874e4bfd1
Natacha <natacha271@gmail.com>
2011-04-12 17:35:53 +0200

    Arborescence sample appli

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'eca6d2e2dda1f063a470b328d6f3f673bc3b7235854d1da16292c13af54c46f574784ed71769b53f90103a4e61d41366691b400bbc537a4d6b09a16132300ba3'
