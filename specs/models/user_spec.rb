24e2332f39d0f9d74c648a2e2d610316f630c17d
Natacha <natacha271@gmail.com>
2011-04-13 21:58:30 +0200

    User model with passwords, and user show page

---
+++ specs/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe User do
+
+  before(:each) do
+      @attr = { :name => "Example User", :email => "user@example.com" }
+  end
+
+  it "should create a new instance given valid attributes" do
+    User.create!(@attr)
+  end
+
+  describe "password encryption" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+  end
+end
